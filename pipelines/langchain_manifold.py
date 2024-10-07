5617a319844a37e951dc51653511feb9b51ab21e
teron131 <t3ron.ay@gmail.com>
2024-10-07 06:51:32 +0000

    Use specific packages for rigorousness

--- pipelines/langchain_manifold.py
+++ pipelines/langchain_manifold.py
@@ -2,12 +2,20 @@ import os
 from typing import Generator, Iterator, List, Union
 
 from langchain.chat_models.base import init_chat_model
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+from langchain_openai.chat_models.azure import AzureChatOpenAI
+from langchain_openai.chat_models.base import ChatOpenAI
 from pydantic import BaseModel
 
 
 class Pipeline:
     class Valves(BaseModel):
-        OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+        OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
+        AZURE_OPENAI_API_KEY: str = os.getenv("AZURE_OPENAI_API_KEY", "")
+        AZURE_OPENAI_ENDPOINT: str = os.getenv("AZURE_OPENAI_ENDPOINT", "")
+        AZURE_OPENAI_DEPLOYMENT_NAME: str = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME", "")
+        AZURE_OPENAI_API_VERSION: str = os.getenv("AZURE_OPENAI_API_VERSION", "")
+        GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
         OPENROUTER_API_KEY: str = os.getenv("OPENROUTER_API_KEY", "")
         pass
 
@@ -30,22 +38,17 @@ class Pipeline:
         return [
             {"id": "gpt-4o", "name": "GPT-4o"},
             {"id": "gpt-4o-mini", "name": "GPT-4o-mini"},
+            {"id": self.valves.AZURE_OPENAI_DEPLOYMENT_NAME, "name": self.valves.AZURE_OPENAI_DEPLOYMENT_NAME},
+            {"id": "gemini-1.5-pro", "name": "Gemini 1.5 Pro"},
+            {"id": "gemini-1.5-flash", "name": "Gemini 1.5 Flash"},
             {"id": "claude-3.5-sonnet", "name": "Claude 3.5 Sonnet"},
         ]
 
     def pipe(self, user_message: str, model_id: str, messages: List[dict], body: dict) -> Union[str, Generator, Iterator]:
         print(f"pipe:{__name__}")
         print(f"model_id: {model_id}")
-        if "claude" in model_id:
-            llm = init_chat_model(
-                model=f"anthropic/{model_id}",  # Avoid making model_id with '/', otherwise it will mess up the FastAPI URL
-                model_provider="openai",
-                base_url="https://openrouter.ai/api/v1",
-                api_key=os.getenv("OPENROUTER_API_KEY", ""),
-            )
-        else:
-            llm = init_chat_model(model=model_id)
 
+        llm = self.get_llm(model_id)
         chain = llm
 
         print(body)
@@ -57,3 +60,25 @@ class Pipeline:
                 return chain.invoke(messages).content
         except Exception as e:
             return f"Error: {e}"
+
+    def get_llm(self, model_id: str):
+        if "azure" in model_id:
+            llm = AzureChatOpenAI(
+                api_key=self.valves.AZURE_OPENAI_API_KEY,
+                azure_endpoint=self.valves.AZURE_OPENAI_ENDPOINT,
+                azure_deployment=self.valves.AZURE_OPENAI_DEPLOYMENT_NAME,
+                api_version=self.valves.AZURE_OPENAI_API_VERSION,
+            )
+        elif "gemini" in model_id:
+            llm = ChatGoogleGenerativeAI(model=model_id, api_key=self.valves.GEMINI_API_KEY)
+        elif "claude" in model_id:
+            llm = ChatOpenAI(
+                model=f"anthropic/{model_id}",  # Avoid making model_id with '/', otherwise it will mess up the FastAPI URL
+                base_url="https://openrouter.ai/api/v1",
+                api_key=self.valves.OPENROUTER_API_KEY,
+            )
+        elif "gpt" in model_id:
+            llm = ChatOpenAI(model=model_id, api_key=self.valves.OPENAI_API_KEY)
+        else:
+            raise ValueError(f"Invalid model_id: {model_id}")
+        return llm
